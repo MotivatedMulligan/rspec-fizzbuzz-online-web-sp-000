9b5aab8c49bbd47bb252a6041443486e3dcba5ad
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-17 21:04:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
-    if (i % 5) == 0 && (i % 3) ==0
-        puts 'FizzBuzz'
-    elsif (i % 5) == 0
-        puts 'Buzz'
-    elsif (i % 3) == 0
-        puts 'Fizz'
-    else
-        puts i
-    end
-end
+
 def fizzbuzz(number)
 if number %3 == 0 && number % 5 == 0
 puts 'FizzBuzz'
@@ -20,6 +10,6 @@ puts 'Fizz'
 elsif number % 5 == 0
 puts 'Buzz'
 else
-puts 
+puts nil
 end
 end

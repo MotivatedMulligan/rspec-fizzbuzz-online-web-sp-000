a2eb79143b60a93f3db0770d97a64e6effb0ece0
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-17 21:02:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz
+    if (i % 5) == 0 && (i % 3) ==0
+        puts 'FizzBuzz'
+    elsif (i % 5) == 0
+        puts 'Buzz'
+    elsif (i % 3) == 0
+        puts 'Fizz'
+    else
+        puts i
+    end
+end

8c1ff679ddf0a770a5fc0c312c447636b12739ea
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-17 21:16:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,13 @@
 
 
 def fizzbuzz(n)
- if n % 3 == 0
+  if n % 3 == 0 && n % 5 == 0
+    return "FizzBuzz"
+ elsif n % 3 == 0
     return "Fizz"
   elsif n % 5 == 0
     return "Buzz"
-  elsif n % 3 == 0 && n % 5 == 0
-    return "FizzBuzz"
+
   else
     return nil
   end

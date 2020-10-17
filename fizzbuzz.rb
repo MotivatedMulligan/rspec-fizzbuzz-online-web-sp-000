8d17946c5c8e21ec273bcf10c51913a52a3a5cfb
Sean Mulligan <sean.paul.mulligan@gmail.com>
2020-10-17 21:05:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,11 +5,11 @@
 def fizzbuzz(number)
   if number %3 == 0 && number % 5 == 0
 puts 'FizzBuzz'
-elsif number % 3 == 0
+  elsif number % 3 == 0
 puts 'Fizz'
-elsif number % 5 == 0
+  elsif number % 5 == 0
 puts 'Buzz'
-else
-puts nil
+  else
+    puts nil
 end
 end
